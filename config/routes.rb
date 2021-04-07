729c94db86bf70957343f6c931938f159ce929cc
Satota2048 <windowformywork@gmail.com>
2021-04-07 12:46:55 +0900

    デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #アクセス時にホーム画面を表示
+  root to: "items#index"
 end
