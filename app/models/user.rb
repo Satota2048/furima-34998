fe371731cab49ee21fb2721dbe68b7ab6d8eec4c
Satota2048 <windowformywork@gmail.com>
2021-04-08 18:58:05 +0900

    deviseのバリデーション記述

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :user_judges
+
+  validates :nickname,           presence: true
+  validates :email,              presence: true 
+  validates :encrypted_password, presence: true
+  validates :family_name,        presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :family_kana_name,   presence: true, format: { with:/\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'}
+  validates :last_kana_name,     presence: true, format: { with:/\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'}
+  validates :birth,              presence: true
+end
