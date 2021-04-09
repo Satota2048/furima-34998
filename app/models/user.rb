1fa92eb55fce2de44f4470f2f87fb8dfe0ab6b34
Satota2048 <windowformywork@gmail.com>
2021-04-09 18:52:57 +0900

    訂正５回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password,           format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-    validates :encrypted_password
     validates :family_name,        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
     validates :last_name,          format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
     validates :family_kana_name,   format: { with:/\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'}
