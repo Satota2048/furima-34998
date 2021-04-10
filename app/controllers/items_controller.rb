1dba5d07e0560c7e6357fc718437db37a2fa7e8e
Satota2048 <windowformywork@gmail.com>
2021-04-10 23:49:54 +0900

    ActiveRecord仮導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  #ActiveRecord不安
+  def message_params
+    params.require(:message).permit(:image).merge(user_id: current_user.id)
+  end
+
 end
