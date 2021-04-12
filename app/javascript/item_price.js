b91fd738b013d7885d1b546f023ef359199ec675
Satota2048 <windowformywork@gmail.com>
2021-04-12 16:36:40 +0900

    不必要な記述削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,17 +1,14 @@
 window.addEventListener('load', () => {
-  console.log("OK");
 
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  const tax = 0.1;
-  const inputTax = Math.floor(inputValue * tax);
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+      const tax = 0.1;
+      const inputTax = Math.floor(inputValue * tax);
     addTaxDom.innerHTML = inputTax;
-  const profitDom = document.getElementById("profit");
-  const profit = inputValue - inputTax;
+    const profitDom = document.getElementById("profit");
+      const profit = inputValue - inputTax;
     profitDom.innerHTML = profit;
 })
 });
-// const priceInput = document.getElementById("item_price");
-// console.log(priceInput);
