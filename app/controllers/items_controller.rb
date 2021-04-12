d9496fe1b268ba5b2052c4c8b80912cd170c5ba9
Satota2048 <windowformywork@gmail.com>
2021-04-12 16:43:50 +0900

    訂正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,12 @@
 class ItemsController < ApplicationController
 
+  before_action :authenticate_user!, only: :new
+
   def index
   end
 
   def new
-    if authenticate_user!
-      @item = Item.new
-    else 
-      redirect_to new_user_session_path
-    end
+    @item = Item.new
   end
 
   def create
@@ -33,4 +31,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :title, :explain, :category_id, :status_id, :cost_day_id, :delivery_fee_id, :send_from_id, :cost_day_id, :how_much, :user).merge(user_id: current_user.id)
   end
 
+  def move_to_new
+    unless authenticate_user!
+      redirect_to  new_user_session_path
+    end
+  end
 end
