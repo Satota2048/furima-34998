88acef1458c265fe772a9f83a50d3079ede2611f
Satota2048 <windowformywork@gmail.com>
2021-04-13 20:54:48 +0900

    createアクションにチェック追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_params, only: [:show, :edit, :update]
   before_action :check, only: [:edit, :update]
 
