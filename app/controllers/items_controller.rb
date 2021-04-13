74e0ba66f8c3f1847a8b554680d7910d644a75df
Satota2048 <windowformywork@gmail.com>
2021-04-13 21:37:00 +0900

    destroyアクションへのチェック追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destory]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_params, only: [:show, :edit, :update, :destroy]
-  before_action :check, only: [:edit, :update]
+  before_action :check, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
