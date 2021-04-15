e3a718b89df8efaf56af497ed61fa1fbac545304
Satota2048 <windowformywork@gmail.com>
2021-04-15 22:14:18 +0900

    単体テストのFactoryBot記述

--- spec/factories/user_judges.rb
+++ spec/factories/user_judges.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
+  require 'randwordjp'
   factory :user_judge do
-    
+    postal_number   {Randwordjp.zip}
+    prefecture_id   {2}
+    municipality    {Randwordjp.address}
+    address         {"なし4-4-4"}
+    building_name   {"虚構バベル塔"}
+    phone_number    {09012345678}
+    user_judge      {1}
   end
 end
