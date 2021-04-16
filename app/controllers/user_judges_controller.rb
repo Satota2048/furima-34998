6a4292026a9badecc3a9ab29ae590efd652df0b3
Satota2048 <windowformywork@gmail.com>
2021-04-16 21:56:47 +0900

    購入機能実装

--- app/controllers/user_judges_controller.rb
+++ app/controllers/user_judges_controller.rb
@@ -13,6 +13,7 @@ class UserJudgesController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
@@ -22,7 +23,7 @@ class UserJudgesController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:postal_number,:prefecture_id,:municipality,:address,:building_name,:phone_number,:user_judge,:user,:item).merge(user_id: current_user.id)
+    params.require(:order).permit(:postal_number,:prefecture_id,:municipality,:address,:building_name,:phone_number).merge(user_id: current_user.id,token: params[:token],item_id: params[:item_id])
   end
 
   def set_params
@@ -37,5 +38,14 @@ class UserJudgesController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: set_params.how_much,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,17 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :user_id,:items_id,:postal_number,:prefecture_id,:municipality,:address,:building_name,:phone_number,:user_judge
+  attr_accessor :user_id,:item_id,:postal_number,:prefecture_id,:municipality,:address,:building_name,:phone_number,:user_judge_id,:token
 
   with_options presence: true do
     validates :postal_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-    validates :municipality,:address,:user_judge,:user_id,:items_id
+    validates :municipality,:address,:user_id,:item_id,:token
     validates :phone_number, numericality: {only_integer: true ,message: "半角数字のみ有効です"}
   end
   validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "10〜11桁の半角数字のみ有効です" }
 
   def save
-    user_judge.create(user_id: user_id,item_id: item_id)
-    send_address.create(postal_number: postal_number,prefecture_id: prefecture_id,municipality: municipality,address: address,building_name: building_name,phone_number: phone_number,user_judge: user_judge)
+    UserJudge.create(user_id: user_id,item_id: item_id)
+    SendAddress.create(postal_number: postal_number,prefecture_id: prefecture_id,municipality: municipality,address: address,building_name: building_name,phone_number: phone_number,user_judge_id: user_judge_id)
   end
 end
