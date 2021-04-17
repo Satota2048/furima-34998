99a85bd5ba5886a7efdb65e7a8866467ecc2afc7
Satota2048 <windowformywork@gmail.com>
2021-04-17 11:54:13 +0900

    エラー表示順番調整

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,10 @@ class Order
   attr_accessor :user_id,:item_id,:postal_number,:prefecture_id,:municipality,:address,:building_name,:phone_number,:user_judge_id,:token
 
   with_options presence: true do
+    validates :token
     validates :postal_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-    validates :municipality,:address,:user_id,:item_id,:token
+    validates :municipality,:address,:user_id,:item_id,
     validates :phone_number, numericality: {only_integer: true ,message: "半角数字のみ有効です"}
   end
   validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "10〜11桁の半角数字のみ有効です" }
